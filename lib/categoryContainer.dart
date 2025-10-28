85f8f475ad5f1e006efba3f686309f80be43bd8d
Carlos <carloscreus@ceroca.cat>
2025-10-28 17:32:56 +0100

    Paginas 1 y 3 avanzadas

--- lib/categoryContainer.dart
+++ lib/categoryContainer.dart
@@ -2,25 +2,22 @@ import 'package:flutter/material.dart';
 
 class categoryContainer extends StatelessWidget {
   final String text;
-  const categoryContainer({super.key,required this.text});
+  const categoryContainer({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-    decoration: BoxDecoration(
-      color: Colors.black,
-        borderRadius: BorderRadius.circular(12)
-                             ),
-         width: 350,
-         height: 100,
-            margin:EdgeInsets.all(16),
-              padding:EdgeInsets.all(8),
-              child:Center(
-                child: Text(text, style: TextStyle(
-                color: Colors.white,
-                fontSize: 20),
-                ),
-              ),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      width: 350,
+      height: 100,
+      margin: EdgeInsets.all(16),
+      padding: EdgeInsets.all(8),
+      child: Center(
+        child: Text(text, style: TextStyle(color: Colors.white, fontSize: 20)),
+      ),
     );
   }
 }
---
+++ lib/valorsglobals.dart
@@ -0,0 +1,19 @@
+class Valorsglobals {
+  //static List values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+  static List valors = [
+    {
+      'nombre': 'Scaffold',
+      'dificultad': 2,
+    },
+    {
+      'nombre': 'GridView',
+      'dificultad': 10,
+    },
+    {
+      'nombre': 'ListView',
+      'dificultad': 6,
+    }
+  ];
+}
